f44551b61ee84320e5010e883a1508d04b7d5589
Codin Moldovanu <kodin94@gmail.com>
2019-11-26 22:33:51 +0200

    Update backup_rpi.sh

--- backup_rpi.sh
+++ backup_rpi.sh
@@ -8,7 +8,7 @@ size="$(ssh pi@192.168.88.226 "sudo blockdev --getsize64 /dev/mmcblk0")"
 
 #Give us some info
 let human_size=size/1073741824
-let expected_time=((human_size*1000)/11)/60
+let expected_time=(human_size*1000/11)/60
 echo "We've got ${human_size}GB to backup which should take about ${expected_time} minutes..."
 
 #Connect to the RaspberryPi and dd the microSD card, pipe it into pv with the $size and output it locally using the $days variable.
