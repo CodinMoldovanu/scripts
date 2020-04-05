e3898cc048fd4908d227fb0242948b1af77591de
Codin Moldovanu <kodin94@gmail.com>
2020-04-05 22:24:29 +0300

    Update backup_rpi.sh
    Rate limiting so connection still allows other stuff to happen, small hack to interpret a string as an int.

--- backup_rpi.sh
+++ backup_rpi.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-EMAIL = "your_email@mail.com"
-ROOT_EMAIL = "root@codin.ro"
-
 #Set the date variable so we have nice file naming
 days="$(date +'%Y-%m-%d')"
 
 #Get the size so we can pass it to pv and display accurate percentage of operation
-size="$(ssh pi@192.168.88.226 "sudo blockdev --getsize64 /dev/mmcblk0")"
+size=$(ssh pi@192.168.88.226 "sudo blockdev --getsize64 /dev/mmcblk0")
 
 #Give us some info
+let rate_limit=11
 let human_size=size/1073741824
-let expected_time=(human_size*1000/11)/60
+let expected_time=(human_size*1000/rate_limit)/60
 echo "We've got ${human_size}GB to backup which should take about ${expected_time} minutes.."
 
 if ssh pi@192.168.88.226 "true"
 then
-	echo -e "From:$(ROOT_EMAIL)\nSubject:RPi Backup ${days}\n\n Pi appears to be ok @ 192.168.88.226, starting backup of ${human_size}GB which should take about ${expected_time} minutes." | ssmtp $EMAIL
+        echo -e "From:root@codin.ro\nSubject:RPi Backup ${days}\n\n Pi appears to be ok @ 192.168.88.226, starting backup of ${human_size}GB which should take about ${expected_time} minutes." | ssmtp kodin94@gmail.com
 else
-	echo -e "From:$(ROOT_EMAIL)\nSubject:RPi Backup ${days}\n\n\ Pi appears to be offline" | ssmtp $EMAIL
+        echo -e "From:root@codin.ro\nSubject:RPi Backup ${days}\n\n\ Pi appears to be offline" | ssmtp kodin94@gmail.com
 fi
 
 #Connect to the RaspberryPi and dd the microSD card, pipe it into pv with the $size and output it locally using the $days variable.
-ssh pi@192.168.88.226 "sudo dd if=/dev/mmcblk0" | pv --size ${size} | dd of=rpi_backups/rpi_img_${days}.img.gz
+ssh pi@192.168.88.226 "sudo dd if=/dev/mmcblk0" | pv -L ${rate_limit}m --size ${size} | dd of=rpi_backups/rpi_img_${days}.img.gz
 
-BACKUP_FILE = rpi_backups/rpi_img_${days}.img.gz
-BACKUP_SIZE = $(stat -c %s $BACKUP_FILE)
+BACKUP_FILE=rpi_backups/rpi_img_${days}.img.gz
+BACKUP_SIZE=$(stat -c %s $BACKUP_FILE)
 
-if [[ BACKUP_SIZE == size ]]
+if [[ $BACKUP_SIZE+0 -eq $size+0 ]]
 then
-	echo -e "From:$(ROOT_EMAIL)\nSubject:RPi Backup ${days} Success\n\n Backup completed successfully." | ssmtp $EMAIL
+        echo -e "From:root@codin.ro\nSubject:RPi Backup ${days} Success\n\n Backup completed successfully. Backup size is ${BACKUP_SIZE}. Image has been saved to {$BACKUP_FILE}." | ssmtp kodin94@gmail.com
 else
-	echo -e "From:$(ROOT_EMAIL)\nSubject:RPi Backup ${days} Success\n\n Did you really think this would work just that easily?" | ssmtp $EMAIL
+        echo -e "From:root@codin.ro\nSubject:RPi Backup ${days} Error\n\n Something went wrong and you have no logging." | ssmtp kodin94@gmail.com
 fi
